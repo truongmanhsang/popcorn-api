21e9cdc88c1fd45aad1b773e2b1b3d9b75797b86
Chris <chris_alderson@hmamail.com>
2016-07-06 09:18:36 +0000

    silly dev, don't use models that way

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,51 +1,49 @@
 // Import the neccesary modules.
 import mongoose from "mongoose";
 
+// The movie schema used by mongoose.
+const MovieSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  imdb_id: String,
+  title: String,
+  year: String,
+  slug: String,
+  synopsis: String,
+  runtime: String,
+  rating: {
+    percentage: Number,
+    watching: Number,
+    votes: Number,
+    loved: Number,
+    hated: Number
+  },
+  country: String,
+  last_updated: Number,
+  latest_episode: {type: Number, default: 0},
+  images: {
+    banner: String,
+    fanart: String,
+    poster: String
+  },
+  genres: [],
+  released: Number,
+  trailer: String,
+  certification: String,
+  torrents: {}
+});
+
 /**
  * @class Movie
  * @classdesc A model object for movies.
  * @memberof module:models/movie
  */
-class Movie extends mongoose.Schema {
+const Movie = mongoose.model("Movie", MovieSchema);
 
-  constructor() {
-    super({
-      _id: {
-        type: String,
-        required: true,
-        index: {
-          unique: true
-        }
-      },
-      imdb_id: String,
-      title: String,
-      year: String,
-      slug: String,
-      synopsis: String,
-      runtime: String,
-      rating: {
-        percentage: Number,
-        watching: Number,
-        votes: Number,
-        loved: Number,
-        hated: Number
-      },
-      country: String,
-      last_updated: Number,
-      images: {
-        banner: String,
-        fanart: String,
-        poster: String
-      },
-      genres: [],
-      released: Number,
-      trailer: String,
-      certification: String,
-      torrents: {}
-    });
-  };
-
-};
-
-// Export the movie model.
-export default mongoose.model("Movie", new Movie());
+// Export the user model.
+export default Movie;
