972f830a5e417f588cc5ddb0f041efa67e0674da
Chris Alderson <chrisalderson@protonmail.com>
2017-03-21 22:56:52 +0100

    use single quotes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,26 @@
 // Import the neccesary modules.
-import del from "del";
-import gulp from "gulp";
-import babel from "gulp-babel";
+import del from 'del';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
 
 /**
  * @description The default build function.
  * @returns {Gulp} - The transpiled source code.
  */
-const build = () => gulp.src("src/**/*.js").pipe(babel()).pipe(gulp.dest("build"));
+function build() {
+  return gulp.src('src/**/*.js')
+              .pipe(babel())
+              .pipe(gulp.dest('build'));
+}
 
 // Delete the `build` directory.
-gulp.task("clean", () => del(["build"]));
+gulp.task('clean', () => del(['build']));
 
 // Transpile the `src` directory with Babel.
-gulp.task("build", ["clean"], build);
+gulp.task('build', ['clean'], build);
 
 // Watch the `src` directory and build when a file changes.
-gulp.task("watch", ["build"], () => gulp.watch("src/**/*.js", ["build"]));
+gulp.task('watch', ['build'], () => gulp.watch('src/**/*.js', ['build']));
 
 // Set the default task as `build`.
-gulp.task("default", ["clean"], build);
+gulp.task('default', ['clean'], build);
