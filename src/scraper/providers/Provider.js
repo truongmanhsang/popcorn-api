2ec44dab6f0d63543645a78ba9850ba08704134a
Chris Alderson <chrisalderson@protonmail.com>
2017-03-23 00:43:41 +0100

    moved class properties and constructor to base provider and added esdoc

--- src/scraper/providers/Provider.js
+++ src/scraper/providers/Provider.js
@@ -1,71 +1,13 @@
-// Import the neccesary modules.
-import { ItemType } from 'butter-provider';
-
-import FactoryProducer from '../resources/FactoryProducer';
-
+/**
+ * Interface for scraping content from various sources.
+ * @interface
+ */
 export default class Provider {
-
-  static ModelTypes = {
-    AnimeMovie: 'animemovie',
-    AnimeShow: 'animeshow',
-    Movie: 'movie',
-    Show: 'show'
-  }
-
-  static Types = {
-    Movie: ItemType.MOVIE,
-    Show: ItemType.TVSHOW
-  }
-
-  /**
-   * Create a BaseTorrentProvider object.
-   * @param {Object} config - The configuration object for the torrent
-   * provider.
-   * @param {Object} config.API - The api of the torrent provider.
-   * @param {String} config.name - The name of the torrent provider.
-   * @param {Model} config.modelType - The model type for the helper.
-   * @param {Object} config.query - The query object for the api.
-   * @param {String} config.type - The type of content to scrape.
-   */
-  constructor({API, name, modelType, query, type} = {}) {
-    const helperFactory = FactoryProducer.getFactory('helper');
-    const modelFactory = FactoryProducer.getFactory('model');
-
-    const model = modelFactory.getModel(modelType);
-
-    /**
-     * The api of the torrent provider.
-     * @type {Object}
-     */
-    this._api = API;
-
-    /**
-     * The name of the torrent provider.
-     * @type {String}
-     */
-    this._name = name;
-
-    /**
-     * The helper object for adding movies.
-     * @type {BaseHelper}
-     */
-    this._helper = helperFactory.getHelper(this._name, model, type);
-
-    /**
-     * The query object for the api.
-     * @type {Object}
-     */
-    this._query = query;
-
-    /**
-     * The type of content to scrape.
-     * @type {String}
-     */
-    this._type = type;
-  }
 
   /**
    * Default method to get content.
+   * @abstract
+   * @throws {Error} - 'Using default method: 'getContent'.
    * @returns {void}
    */
   getContent() {
@@ -74,6 +16,8 @@ export default class Provider {
 
   /**
    * Default method to extract content.
+   * @abstract
+   * @throws {Error} - 'Using default method: '_extractContent'.
    * @returns {void}
    */
   _extractContent() {
@@ -82,6 +26,8 @@ export default class Provider {
 
   /**
    * Default method to get content data.
+   * @abstract
+   * @throws {Error} - 'Using default method: '_getContentData'.
    * @returns {void}
    */
   _getContentData() {
@@ -90,6 +36,8 @@ export default class Provider {
 
   /**
    * Default method to attach a torrent.
+   * @abstract
+   * @throws {Error} - 'Using default method: '_attachTorrent'.
    * @returns {void}
    */
   attachTorrent() {
@@ -98,6 +46,8 @@ export default class Provider {
 
   /**
    * Default method to get all content.
+   * @abstract
+   * @throws {Error} - 'Using default method: '_getAllContent'.
    * @returns {void}
    */
   _getAllContent() {
@@ -106,6 +56,8 @@ export default class Provider {
 
   /**
    * Default method to get all torrents.
+   * @abstract
+   * @throws {Error} - 'Using default method: '_getAllTorrents'.
    * @returns {void}
    */
   _getAllTorrents() {
@@ -114,6 +66,8 @@ export default class Provider {
 
   /**
    * Default method to search for torrents.
+   * @abstract
+   * @throws {Error} - 'Using default method: 'search'.
    * @returns {void}
    */
   search() {
