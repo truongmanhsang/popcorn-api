689b25e225d48ba7a3110a69afab25f9efef5b54
Chris Alderson <chrisalderson@protonmail.com>
2017-03-23 00:42:47 +0100

    added esdoc for context

--- src/scraper/Context.js
+++ src/scraper/Context.js
@@ -1,16 +1,31 @@
 // Import the neccesary modules.
 import Provider from './providers/Provider';
 
+/** Class for the scraper context. */
 export default class Context {
 
+  /** Create a new Context class. */
   constructor() {
+    /**
+     * Set the default provider strategy.
+     * @type {Provider}
+     */
     this._provider = new Provider();
   }
 
+  /**
+   * Execute the current provider strategy.
+   * @returns {void}
+   */
   execute() {
     return this._provider.search();
   }
 
+  /**
+   * Set the current provider.
+   * @param {Provider} provider - The provider to set.
+   * @returns {void}
+   */
   set provider(provider) {
     this._provider = provider;
   }
