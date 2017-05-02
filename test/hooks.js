7ef2a1da63ed9cbf4edb11a7606345a783bb6609
ChrisAlderson <chrisalderson@protonmail.com>
2017-05-02 14:58:47 +0200

    tests working with mockup data

--- test/hooks.js
+++ test/hooks.js
@@ -1,16 +1,22 @@
 // Import the neccesary modules.
 import hooks from 'hooks';
+import path from 'path';
 
+import Logger from '../src/config/Logger';
 import Util from '../src/Util';
 
-hooks.beforeAll((transaction, done) => {
-  const animemovie = Util.importCollection('anime', './test/movie.json');
-  const animeshow = Util.importCollection('anime', './test/show.json');
-  const movie = Util.importCollection('movie', './test/movie.json');
-  const show = Util.importCollection('show', './test/show.json');
+hooks.beforeAll((t, done) => {
+  global.tempDir = path.join(process.cwd(), 'tmp');
+  Logger.getLogger('winston', undefined, true);
 
-  Promise.all([animemovie, animeshow, movie, show])
-    .then(done).catch(done);
+  const animemovie = Util.Instance.importCollection('anime', './test/movie.json');
+  const animeshow = Util.Instance.importCollection('anime', './test/show.json');
+  const movie = Util.Instance.importCollection('movie', './test/movie.json');
+  const show = Util.Instance.importCollection('show', './test/show.json');
+
+  return Promise.all([animemovie, animeshow, movie, show])
+    .then(done)
+    .catch(done);
 });
 
 hooks.before('Logs > Error Logs > Get Error Logs', t => t.skip = true);
