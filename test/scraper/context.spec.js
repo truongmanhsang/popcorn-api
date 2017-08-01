1e52bd1f7f50089785e90bcb94542d88c4dd0574
Chris Alderson <chrisalderson@protonmail.com>
2017-08-01 11:27:04 +0200

    add context unit tests

--- test/scraper/context.spec.js
+++ test/scraper/context.spec.js
@@ -3,7 +3,8 @@
 /* eslint-disable no-unused-expressions */
 import { expect } from 'chai'
 
-// import Context from '../../src/scraper/Context'
+import BaseProvider from '../../src/scraper/providers/BaseProvider'
+import Context from '../../src/scraper/Context'
 
 /**
  * @test {Context}
@@ -11,24 +12,35 @@ import { expect } from 'chai'
  */
 describe('Context', () => {
 
-  // let context: Context
+  let context: Context
 
   /**
    * Hook for setting up the Context tests.
    * @type {Function}
    */
   before(() => {
-    // context = new Context()
+    context = new Context()
   })
 
-  it('should test something', () => {
-    expect(true).to.be.true
+  /** @test {Context#_provider} */
+  it('should check if Context has a _provider', () => {
+    expect(context._provider).to.be.an('object')
   })
 
-  /**
-   * Hook for teaing down the Context tests.
-   * @type {Function}
-   */
-  after(() => {})
+  /** @test {Context#execute} */
+  it('should throw an error when executing the default provider', () => {
+    expect(context.execute.bind(context)).to
+      .throw('Using default method: \'search\'')
+  })
+
+  /** @test {Context#_provider} */
+  it('should check if Context has a _provider', () => {
+    const current = context._provider
+    const baseProvider = new BaseProvider({})
+    context.provider = baseProvider
+
+    expect(context._provider).to.not.equal(current)
+    expect(context._provider).to.equal(baseProvider)
+  })
 
 })
